4d1e830c77bb4e38c30b896ab49fa1697f84e3df
wenhaoyu <2853379081@qq.com>
2021-02-01 18:59:51 +0800

    增加集群和kvm联动

--- ceph_auto_install.sh
+++ ceph_auto_install.sh
@@ -378,3 +378,6 @@ echo "请将如下内容加入访问dashbrod——web的host文件，否则可�
 echo -e "\n"
 cat /tmp/ceph_host_info |sed '1,2d'
 echo -e "\n"
+echo -e "$ceph_segment \c" > /tmp/2 #将网段信息临时存储，方便其他脚本调用
+echo ”如果你已确定ceph部署成功，同时魔方云也已经部署成功，魔方云节点内核已升级到5以上可以用以下脚本自动初始化魔方云ceph环境“
+echo ”请在本机执行  ”wget -O mofang_ceph_env “ “
--- mofang_ceph_env.sh
+++ mofang_ceph_env.sh
@@ -1,19 +1,90 @@
 #!/bin/bash
-cat > /root/ceph_ansible/ceph_initenv.yml <<EOF
+echo "该脚本只能在ceph脚本后使用，同时计算节点已换好内核和已正常安装魔方云"
+echo "有几个魔方云计算节点"
+read mofang_num
+mofang_num_1=$mofang_num
+a=0
+echo "[mofang]" > /root/ceph_ansible/mofang_hosts
+while [[ $mofang_num > "0" ]]; do
+    let "a++"
+    let "mofang_num--"
+    echo "请输入第$a 个魔方云计算节点IP"
+    read mofang$a
+    echo "请输入第$a 个魔方云计算节点密码"
+    read mofang_passwd$a
+    echo "mofang$a" >> /root/ceph_ansible/mofang_hosts
+done
+#实现到计算节点免密
+rpm -q sshpass &> /dev/null || yum install sshpass -y &> /dev/null
+sed -i '/Checking ask/c StrictHostKeyChecking no' /etc/ssh/ssh_config
+[ -f /root/.ssh/id_rsa ] || ssh-keygen -P "" -f /root/.ssh/id_rsa &> /dev/null
+b=0
+while [[ $mofang_num_1 > "0" ]]; do
+  let "b++"
+  let "mofang_num_1--"
+  mofang_ip=`eval echo "$"mofang$b""`
+  mofang_passwd=`eval echo "$"mofang_passwd$b""`
+  sshpass -p $mofang_ip ssh-copy-id -i  /root/.ssh/id_rsa.pub $mofang_passwd &> /dev/null
+done
+#在ceph主节点获取 ceph.client.admin.keyring  ceph.conf  ceph.pub 三位大哥
+ansible ceph_master -m copy -a "src=/etc/ceph/ dest=/root/ceph_ansible/ceph_config/ force=yes backup=yes"
+ceph_fsid=`cat /root/ceph_ansible/ceph_config/ceph.conf | grep fsid|awk -F ' ' '{print $3}'`
+ceph_segment=`cat /tmp/2`
+node0_ip=`cat /tmp/ceph_host_info |grep node0|awk '{print $1}'`
+node1_ip=`cat /tmp/ceph_host_info |grep node1|awk '{print $1}'`
+node2_ip=`cat /tmp/ceph_host_info |grep node2|awk '{print $1}'`
+ceph_key=`cat /root/ceph_ansible/ceph_config/ceph.client.admin.keyring |grep key|awk '{print $3}'`
+cat > /root/ceph_ansible/ceph_config/ceph.conf << EOF
+[global]
+fsid = $ceph_fsid
+public_network = $ceph_segment
+mon_initial_members = node0,node1,node2
+mon_host = $node0_ip,$node1_ip,$node2_ip
+auth_cluster_required = cephx
+auth_service_required = cephx
+auth_client_required = cephx
+EOF
+# 生成xml绑定文件
+cat > /root/ceph_ansible/secret.xml.j2 <<EOF
+<secret ephemeral='no' private='no'>
+  <uuid>$ceph_fsid</uuid>
+  <usage type='ceph'>
+        <name>client.admin secret</name>
+  </usage>
+</secret>
+EOF
+#生成热迁移文件
+cat > /root/ceph_ansible/libvirtd.conf << EOF
+listen_tls = 0
+listen_tcp = 1
+listen_addr = "0.0.0.0"
+auth_tcp = "none"
+EOF
+#生效配置到各mofang节点
+cat > /root/ceph_ansible/ceph_initenv_mofang.yml <<EOF
 ---
 - hosts: mofang
   vars:
     max_clients: 200
   user: root
   tasks:
-  - name: 魔方安装ceph基础环境
+  - name: 计算节点安装ceph基础环境
     yum: name={{ item }} state=latest
     loop:
       - epel-release
       - ceph-common
-  - name:
+  - name: 传送对应配置文件到计算节点
+    copy: src=/root/ceph_ansible/ceph_config/ dest=/etc/ceph/   backup=yes
+  - name: 传送xml文件到计算节点
+    template: src=/root/ceph_ansible/secret.xml.j2 dest=/etc/ceph/secret.xml
+  - name: 绑定安全设置
+    shell: virsh secret-define --file /etc/ceph/secret.xml
+  - name: 设置安全值
+    shell: virsh secret-set-value --secret $ceph_fsid --base64 $ceph_key
+  - name: 开启热迁移功能
+    copy: src=/root/ceph_ansible/libvirtd.conf dest=/etc/libvirt/libvirtd.conf backup=yes
+  - name: 重启libvirtd
+    shell: systemctl restart libvirtd
+
 EOF
-
-
-
-ansible mofang -m copy -a src=/etc/ceph/
+ansible-playbook -i /root/ceph_ansible/mofang_hosts /root/ceph_ansible/ceph_initenv_mofang.yml
